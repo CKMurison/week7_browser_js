e7c98d93eef20fe094c74d2e2db5aa37615217b1
Cameron Murison <cmuris4@gmail.com>
2023-06-01 10:43:17 +0100

    modified test and notesView class

--- notes_app/notesView.js
+++ notes_app/notesView.js
@@ -1,4 +1,19 @@
 class notesView {
+   constructor(model) {
+      this.model = model;
+      this.mainContainerEl = document.querySelector('#main-container');
+  
+      document.querySelector('#add-note-btn').addEventListener('click', () => {
+        const newNote = document.querySelector('#add-note-input').value;
+        this.addNewNote(newNote);
+      });
+    }
+  
+    addNewNote(newNote) {
+      this.model.addNote(newNote);
+      this.displayNotes();
+    }
+
    displayItems(noteItems) {
     noteItems.forEach((noteItem) => {
      const newElement = document.createElement('div');
--- notes_app/notesView.test.js
+++ notes_app/notesView.test.js
@@ -3,6 +3,7 @@
  */
 
 const notesView = require('./notesView');
+const notesModel = require('./notesModel');
 const fs = require('fs');
 
 describe('notesModel view', () => {
@@ -13,5 +14,22 @@ describe('notesModel view', () => {
     const notesItemElement = document.querySelectorAll('div.notes-item');
     expect(notesItemElement.length).toBe(1);
     expect(notesItemElement[0].innerText).toBe('This is a test Note item!');
+  });
+  it('adds a new note', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+
+    const model = new NotesModel();
+    const view = new NotesView(model);
+      // 1. Fill the input
+    const input = document.querySelector('#add-note-input');
+    input.value = 'My new amazing test note';
+
+    // 2. Click the button
+    const button = document.querySelector('#add-note-btn');
+    button.click();
+
+    // 3. The note should be on the page
+    expect(document.querySelectorAll('div.note').length).toEqual(1);
+    expect(document.querySelectorAll('div.note')[0].textContent).toEqual('My new amazing test note');
   });
 });
